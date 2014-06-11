29e56087b4a09a7aa38e6a9de22cc24db3e0a588
Risa <lisaffang@gmail.com>
2014-06-11 07:55:38 -0700

    lab 3 attempt 2

--- my_first_app/my_first_app.py
+++ my_first_app/my_first_app.py
@@ -1,10 +1,20 @@
 import flask, flask.views
+import os 
+
 app = flask.Flask(__name__) # i don't understand name
+
+app.secret_key = "lettuce"
 
 class View(flask.views.MethodView):	# what's a class?
 	def get(self):
 		return flask.render_template('index.html')
 
-app.add_url_rule('/', view_func=View.as_view('main'))	# i have no idea what's happening
+	def post(self):
+		result = eval (flask.request.form['expression'])
+		flask.flash(result)
+		return self.get()
+
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])	
+# i have no idea what's happening
 app.debug = True
 app.run()	# is this invoking the class?
